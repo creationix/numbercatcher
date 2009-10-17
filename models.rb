7f1d8c8e414b7460516e06741fb65938026ad918
Tim Caswell <tim@creationix.com>
2009-10-17 12:09:51 -0500

    Update the models to not be namespaced

--- models.rb
+++ models.rb
@@ -7,75 +7,72 @@ require 'digest/md5'
 # Set up our app to use a local file as a sqlit3 database
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/db.sqlite3")
 
-module NumberCatcher
-  
-  # This is the login information and profile for each user of the system.
-  # Hash is a hash of the user's password used for authentication.
-  # Note that password_hash is private, it can be set via password=
-  class User
-    include DataMapper::Resource
-    property :id,            Serial
-    property :email,         String
-    property :name,          String
-    property :password_hash, String,  :length => 32, :accessor => :private
-    property :is_admin?,     Boolean, :default  => false
 
-    def password= (pass)
-      # Not super high security, but a start.
-      self.password_hash = Digest::MD5.hexdigest("Number#{pass}Catcher")
-    end
-    
-    has n, :reservations
+# This is the login information and profile for each user of the system.
+# Hash is a hash of the user's password used for authentication.
+# Note that password_hash is private, it can be set via password=
+class User
+  include DataMapper::Resource
+  property :id,            Serial
+  property :email,         String
+  property :name,          String
+  property :password_hash, String,  :length => 32, :accessor => :private
+  property :is_admin?,     Boolean, :default  => false
+
+  def password= (pass)
+    # Not super high security, but a start.
+    self.password_hash = Digest::MD5.hexdigest("Number#{pass}Catcher")
   end
   
-  # This represents a set of numbers.  For example, this could be the list of 
-  # available ports on a given machine.
-  class Set
-    include DataMapper::Resource
-    property :id,   Serial
-    property :name, String
-    property :type, Enum.new("int", "hex")
-  
-    has n, :ranges
-    has n, :reservations
-  end
+  has n, :reservations
+end
 
-  # A number set has several ranges of avialable numbers, this represents a
-  # single range for a set.
-  class Range
-    include DataMapper::Resource
-    property :id,  Serial
-    property :min, Integer
-    property :max, Integer
-  
-    belongs_to :set
-  end
+# This represents a set of numbers.  For example, this could be the list of 
+# available ports on a given machine.
+class Set
+  include DataMapper::Resource
+  property :id,   Serial
+  property :name, String
+  property :type, Enum.new("int", "hex")
 
-  # This is the reservation for a particular number for a particular set for a 
-  # particular user.
-  class Reservation
-    include DataMapper::Resource
-    property :id,     Serial
-    property :number, Integer
-  
-    belongs_to :set
-    belongs_to :user
-  end
+  has n, :sequences
+  has n, :reservations
+end
 
-  # A log of all reservations and releases is stored in this table so that we
-  # have a history of all transactions in the system.
-  class Log
-    include DataMapper::Resource
-    property :id,         Serial
-    property :action,     Enum.new("reserve", "release")
-    property :note,       String
-    property :number,     Integer
-    property :created_at, DateTime
-    belongs_to :set
-    belongs_to :owner,    :model => "User"
-    belongs_to :operator, :model => "User"
+# A number set has several sequences of avialable numbers, this represents a
+# single sequence for a set.
+class Sequence
+  include DataMapper::Resource
+  property :id,  Serial
+  property :min, Integer
+  property :max, Integer
 
-  end
+  belongs_to :set
+end
+
+# This is the reservation for a particular number for a particular set for a 
+# particular user.
+class Reservation
+  include DataMapper::Resource
+  property :id,     Serial
+  property :number, Integer
+
+  belongs_to :set
+  belongs_to :user
+end
+
+# A log of all reservations and releases is stored in this table so that we
+# have a history of all transactions in the system.
+class Log
+  include DataMapper::Resource
+  property :id,         Serial
+  property :action,     Enum.new("reserve", "release")
+  property :note,       String
+  property :number,     Integer
+  property :created_at, DateTime
+  belongs_to :set
+  belongs_to :owner,    :model => "User"
+  belongs_to :operator, :model => "User"
 
 end
 
